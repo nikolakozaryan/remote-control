a7fb0850caa8a8df013b415393bc2350faae7b08
Nikolay <kolyak770@gmail.com>
2023-01-19 19:27:19 +0300

    feat: create WebSocket server

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
-import { httpServer } from "./src/http_server/";
-import { mouse } from "@nut-tree/nut-js";
-
-const HTTP_PORT = 8181;
+import { WebSocketServer } from 'ws';
+import { HTTP_PORT, WS_PORT } from './src/constants/constants';
+import { httpServer } from './src/http_server/';
+import { messageHandler } from './src/utils/messageHandler';
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: WS_PORT });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    messageHandler(message, ws);
+  });
+
+  ws.on('close', () => {
+    console.log(`Client disconnected`);
+  });
+
+  ws.on('error', (error) => {
+    console.log('Error: ', error);
+  });
+});
