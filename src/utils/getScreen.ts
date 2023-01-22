8869ef4df0c33750e13c5479ad15b3dd74ced090
Nikolay <kolyak770@gmail.com>
2023-01-22 15:30:46 +0300

    feat: add screenshot region validation

--- src/utils/getScreen.ts
+++ src/utils/getScreen.ts
@@ -3,8 +3,26 @@ import Jimp from 'jimp';
 import { Actions } from '../constants/constants';
 import { Duplex } from 'stream';
 
+const isValidScreen = async (x: number, y: number): Promise<boolean> => {
+  const screenWidth = await screen.width();
+  const screenHeight = await screen.height();
+  const [upperX, upperY, lowerX, lowerY] = [x + 100, y + 100, x - 100, y - 100];
+  const isInScreen =
+    upperX <= screenWidth &&
+    lowerX >= 0 &&
+    upperY <= screenHeight &&
+    lowerY >= 0;
+  return isInScreen;
+};
+
 export const getScreen = async (stream: Duplex): Promise<string> => {
   const { x, y } = await mouse.getPosition();
+  const validScreen = await isValidScreen(x, y);
+
+  if (!validScreen) {
+    return 'Error: screenshot area is out of screen';
+  }
+
   const left = x - 100;
   const top = y - 100;
   const region = new Region(left, top, 200, 200);
